dd139103ef6f06ae3be23c38330fd954c7ad977f
Mike Maher <mike.maher@exchequer.ie>
2018-09-19 16:25:01 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import Contacts from './components/Contact/Contacts';
+import AddContact from './components/Contact/AddContact';
+import Header from './components/Layout/Header';
+import { Provider } from './context';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Provider>
+        <div className="App">
+          <Header branding="Contact Manager" />
+          <div className="container">
+            <AddContact />
+            <Contacts />
+          </div>
+        </div>
+      </Provider>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Consumer } from '../../context';
+
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
+
+  onDeleteClick = (id, dispatch) => {
+    dispatch({ type: 'DELETE_CONTACT', payload: id });
+  }
+
+  render() {
+    const { id, name, email, phone } = this.props.contact;
+    const { showContactInfo } = this.state;
+
+    return (
+      <Consumer>
+        {value => {
+          const { dispatch } = value;
+          return (
+            <div className="card card-body mb-3">
+              <h4>{name}{' '}
+                <i style={{ cursor: 'pointer' }} onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo })} className="fas fa-sort-down" />
+                <i onClick={this.onDeleteClick.bind(this, id, dispatch)} className="fas fa-times" style={{ cursor: 'pointer', color: 'red', float: 'right' }} />
+              </h4>
+              {showContactInfo ? (<ul className="list-group">
+                <li className="list-group-item">Email: {email}</li>
+                <li className="list-group-item">Phone: {phone}</li>
+              </ul>) : null}
+            </div>
+            )
+          }
+        }
+      </Consumer>
+    )
+  }
+}
+
+Contact.propTypes = {
+  contact: PropTypes.object.isRequired
+}
+
+export default Contact;
