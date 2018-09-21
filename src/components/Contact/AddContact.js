476e5ccd3becbf610be28850f076287261be0460
Mike Maher <mike.maher@exchequer.ie>
2018-09-21 11:52:01 +0100

    refactored requests using async await

--- src/components/Contact/AddContact.js
+++ src/components/Contact/AddContact.js
@@ -12,7 +12,7 @@ class AddContact extends Component {
     errors: {}
   }
 
-  onSubmit = (dispatch, e) => {
+  onSubmit = async (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
 
@@ -28,14 +28,15 @@ class AddContact extends Component {
       this.setState({ errors: { phone: 'Phone is required' } });
       return;
     }
+    
     const newContact= {
       name,
       email,
       phone,
     }
 
-    axios.post('https://jsonplaceholder.typicode.com/users/', newContact)
-      .then(res => dispatch({type: 'ADD_CONTACT', payload: res.data}));
+    const res = await axios.post('https://jsonplaceholder.typicode.com/users/', newContact);
+    dispatch({type: 'ADD_CONTACT', payload: res.data});
 
 
     this.setState({
